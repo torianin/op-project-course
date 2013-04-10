6636a4dcdc8610f5be3f3bea61f012e21c8b7811
Torianin <tori@robert-i.com>
2013-04-10 02:27:04 +0200

    Working sockets

--- server.rb
+++ server.rb
@@ -1,5 +1,32 @@
-require 'sinatra'
+require 'rubygems'
+require 'em-websocket'
+require 'sinatra/base'
+require 'thin'
 
-get '/' do
-  "Hello World!"
+@sockets = []
+EventMachine.run do
+  class App < Sinatra::Base
+      get '/' do
+          erb :index
+      end
+  end
+
+  EventMachine::WebSocket.start(:host => '0.0.0.0', :port => 8080) do |ws| # <-- Added |ws|
+      ws.onopen {
+      	  @sockets << ws
+          ws.send "connected!!!!"
+      }
+
+      ws.onmessage { |msg|
+          puts "#{msg} podlaczony"
+          @sockets.each {|s| s.send msg}
+      }
+
+      ws.onclose do
+          ws.send "WebSocket closed"
+		  @sockets.delete ws
+      end
+
+  end
+  App.run!({:port => 3000})
 end
